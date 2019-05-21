cc045712c49c604404043d35eeacb1a6b395ebd9
Inès Casenave <Ins Casenave@LAPTOP-6QGIRISL>
2019-05-21 11:10:45 +0200

    commit3 tests

---
+++ Test/jpu2016/craft/AssassinTest.java
@@ -0,0 +1,37 @@
+package jpu2016.craft;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import jpu2016.hero.Human;
+
+public class AssassinTest extends CraftTest{
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		this.craft = new Assassin(new Human());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
